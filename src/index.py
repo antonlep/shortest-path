3d473952d4048c1d25c3f59b0137bbc8a7ffa71b
antonlep <antonleppa@gmail.com>
2022-03-23 21:55:53 +0200

    save data as list

--- src/index.py
+++ src/index.py
@@ -5,7 +5,7 @@ def main():
     image.import_map('data/Berlin_0_256.map')
     image.resize(512, 512)
     image.save()
-
+    data = image.data
 
 if __name__ == "__main__":
     main()
--- src/map_image.py
+++ src/map_image.py
@@ -7,19 +7,26 @@ class MapImage:
         self.height = height
         self.mode = 'RGB'
         self.im = Image.new(self.mode, (self.width, self.height))
+        self.data = []
 
     def import_map(self, map):
         self.map = map
+        data = []
         with open(map) as f:
             for x, line in enumerate(f):
                 if x < 4:
                     continue
                 x = x - 4
+                row = []
                 for y, symbol in enumerate(line):
                     if symbol == ".":
                         self.im.putpixel((x, y), (0, 0, 155))
+                        row.append(symbol)
                     elif symbol == "@":
                         self.im.putpixel((x, y), (0, 255, 0))
+                        row.append(symbol)
+                data.append(row)
+        self.data = data
 
     def resize(self, width, height):
         self.im = self.im.resize((width, height))
