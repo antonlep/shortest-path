fb1d94cdbe58354077faa27ae588c4c5bc41c168
antonlep <antonleppa@gmail.com>
2022-03-26 11:10:16 +0200

    simpler cmd arguments

--- src/index.py
+++ src/index.py
@@ -8,15 +8,15 @@ def main():
     end = (int(sys.argv[4]), int(sys.argv[5]))
     image = MapImage(256, 256)
     output_size = (512, 512)
-    image.import_map(map)
-    image.save(output_size, "data/original")
+    image.import_map("data/" + map + ".map")
+    image.save(output_size, "data/" + map + "_original")
     graph = image.create_graph()
     dijkstra = Dijkstra()
     route, shortest_distance, visited = dijkstra.calculate_distance(graph, start, end)
     print("shortest distance: ", shortest_distance)
     image.add_route(visited, (100,0,0))
     image.add_route(route, (0,0,0))
-    image.save(output_size, "data/route")
+    image.save(output_size, "data/" + map + "_route")
 
 if __name__ == "__main__":
     main()
